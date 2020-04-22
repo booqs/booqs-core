4b919e31e876629de1873a95405c128522ace1fe
ashakirzianov <ashakirzianov@icloud.com>
2020-04-22 17:36:15 -0300

    Define booq model

---
+++ src/model/booq.ts
@@ -0,0 +1,57 @@
+export type BooqPath = number[];
+
+export type BooqStringNode = {
+    node: 'string',
+    content: string,
+};
+
+export type BooqNodeAttrs = {
+    [name in string]?: string;
+};
+export type BooqNodeStyle = {
+    [name in string]?: string;
+};
+export type BooqElementNode = {
+    node?: undefined,
+    name: string,
+    id?: string,
+    style?: BooqNodeStyle,
+    children?: BooqNode[],
+    attrs?: BooqNodeAttrs,
+}
+
+export type BooqNode = BooqStringNode | BooqElementNode;
+
+export type BooqImage = {
+    image: 'url', url: string,
+} | {
+    image: 'base64', base64: string,
+};
+
+export type TableOfContentsItem = {
+    title: string | undefined,
+    level: number,
+    path: BooqPath,
+    position: number,
+};
+export type TableOfContents = {
+    title: string | undefined,
+    items: TableOfContentsItem[],
+    length: number,
+};
+
+export type BooqMeta = {
+    title?: string,
+    authors?: string[],
+    languages?: string[],
+    subjects?: string[],
+    license?: string,
+    extra: {
+        [name in string]?: string;
+    },
+};
+export type Booq = {
+    nodes: BooqNode[],
+    meta: BooqMeta,
+    toc: TableOfContents,
+}
