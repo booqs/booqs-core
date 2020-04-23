d7c9f32f8e6995950391942c1fae79e7f95b4e21
ashakirzianov <ashakirzianov@icloud.com>
2020-04-23 15:19:45 -0300

    Remove 'result'

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,2 +1,1 @@
 export * from './booq';
-export * from './result';
--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -2,11 +2,15 @@ export function assertNever(x: never) {
     return x;
 }
 
-export function flatten<T>(arr: Array<T[] | undefined>): T[] {
+export function flatten<T>(arr: T[][]): T[] {
     return arr.reduce<T[]>(
-        (res, a) => a !== undefined && a.length > 0
+        (res, a) => a.length > 0
             ? [...res, ...a]
             : res,
         [],
     );
+}
+
+export function filterUndefined<T>(arr: Array<T | undefined>): T[] {
+    return arr.filter(x => x !== undefined) as T[];
 }
