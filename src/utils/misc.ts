3e71a50d482e0cdb0e7816bb744c90be403dd503
ashakirzianov <ashakirzianov@icloud.com>
2020-04-22 23:40:14 -0300

    'flatten' util

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,3 +1,12 @@
 export function assertNever(x: never) {
     return x;
 }
+
+export function flatten<T>(arr: Array<T[] | undefined>): T[] {
+    return arr.reduce<T[]>(
+        (res, a) => a !== undefined && a.length > 0
+            ? [...res, ...a]
+            : res,
+        [],
+    );
+}
