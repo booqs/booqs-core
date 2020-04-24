7e69352bd49e02705fa9a3e5f5c84828ef3adc91
ashakirzianov <ashakirzianov@icloud.com>
2020-04-23 21:31:13 -0300

    Model changes

--- src/model/booq.ts
+++ src/model/booq.ts
@@ -1,45 +1,19 @@
 export type BooqPath = number[];
 
-export type BooqIgnoreNode = {
-    node: 'ignore',
-    comment?: string,
-    attrs?: undefined,
-}
-export type BooqStringNode = {
-    node: 'text',
-    content: string,
-    attrs?: undefined,
-};
-export type BooqImageNode = {
-    node: 'image',
-    image: BooqImage,
-    id?: string,
-    attrs?: BooqNodeAttrs,
-};
-
 export type BooqNodeAttrs = {
     [name in string]?: string;
 };
 export type BooqNodeStyle = {
     [name in string]?: string;
 };
-export type BooqElementNode = {
-    node?: undefined,
-    name: string,
+export type BooqNode = {
+    name?: string,
     id?: string,
     style?: BooqNodeStyle,
     children?: BooqNode[],
     attrs?: BooqNodeAttrs,
+    content?: string,
 }
-
-export type BooqNode =
-    | BooqStringNode | BooqElementNode | BooqIgnoreNode | BooqImageNode;
-
-export type BooqImage = {
-    image: 'url', url: string,
-} | {
-    image: 'base64', base64: string,
-};
 
 export type TableOfContentsItem = {
     title: string | undefined,
@@ -54,17 +28,14 @@ export type TableOfContents = {
 };
 
 export type BooqMeta = {
-    title?: string,
-    authors?: string[],
-    languages?: string[],
-    subjects?: string[],
-    license?: string,
-    extra?: {
-        [name in string]?: string;
-    },
+    [name in string]?: string | string[];
+};
+export type BooqImages = {
+    [src in string]: string | undefined;
 };
 export type Booq = {
     nodes: BooqNode[],
     meta: BooqMeta,
     toc: TableOfContents,
+    images: BooqImages,
 }
