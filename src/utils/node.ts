be8e06cd86649626b7a31173d832c6481fa4e07c
ashakirzianov <ashakirzianov@icloud.com>
2020-04-24 00:41:42 -0300

    Fix 'iterateNodes'

--- src/utils/node.ts
+++ src/utils/node.ts
@@ -1,21 +1,22 @@
 import { BooqNode, BooqPath } from '../model';
 
-export type BooqNodeIterator = {
+type BooqNodeIteratorValue = {
     node: BooqNode,
     path: BooqPath,
     position: number,
 };
-export function* iterateNodes(nodes: BooqNode[], path: BooqPath = [], position = 0): Generator<BooqNodeIterator, number> {
-    let idx = 0;
+export function* iterateNodes(nodes: BooqNode[], path: BooqPath = [0], position = 0): Generator<BooqNodeIteratorValue, number> {
+    const head = path.slice(0, path.length - 1);
+    let idx = path[path.length - 1];
     for (const node of nodes) {
-        const nextPath = [...path, idx];
+        const nextPath = [...head, idx];
         yield {
             node,
             position,
             path: nextPath,
         };
         if (node.children) {
-            const children = iterateNodes(node.children, nextPath, position);
+            const children = iterateNodes(node.children, [...nextPath, 0], position);
             let child = children.next();
             while (!child.done) {
                 yield child.value;
